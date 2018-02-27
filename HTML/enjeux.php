9b2a421cbc82171708bc6610772da9e7e5981fc8
Unknown <egoumillout@ensc.fr>
2018-02-27 21:04:44 +0100

    [Enjeux + CSS + Historique] ajout Enjeux + modification CSS pour la ligne

---
+++ HTML/enjeux.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title> CLAQH - Enjeux</title>
+		<link rel="icon" href="../IMG/logoTransparentSansTitre.png"  type="image/png">
+		<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="../CSS/style.css">
+	</head>
+
+
+	<body>
+        <?php include_once 'navigation.html'; ?>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="row">
+						<div class="col-sm-1">
+							<h1> Enjeux </h1>
+						</div>
+						<div class="col-sm-11">
+							<br/><hr>
+						</div>
+					</div>
+					<p>
+					Le projet CLAQH comporte de nombreux enjeux. Il vise à répondre à différentes questions afin d’évaluer un risque potentiel de contamination pour l’environnement et les populations locales. Ces questions peuvent être énoncées comme suit :  Pourquoi certaines espèces de poissons et certains lacs semblent plus contaminés que d’autres ? Comment se répartit ce mercure au sein de chaque écosystème ? Quelle est l’origine de la contamination de ces poissons par le mercure ? Quels impacts cette pollution peut avoir sur la population humaine ? Quelle est la perception du risque par les consommateurs ? Le projet comportant plusieurs volets, chacun d’entre eux possède des objectifs précis.				
+					</p>
+				</div>
+			</div>
+		</div>
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="../bootstrap/js/bootstrap.min.js"></script>
+
+	</body>
+
+</html>
--- HTML/historique.php
+++ HTML/historique.php
@@ -26,11 +26,11 @@
 						</div>
 					</div>
 					<p>
-						Le projet prend ses origines au début des années 2010. Tout d’abord, à l’automne 2011, 25 kilos de poissons sont prélevés dans le lac d’Hourtin-Carcans, sous l’impulsion de l’association de Pêche de Hourtin, et confiés au Bureau Veritas pour des analyses. Il s’avère que les quantités de mercure retrouvées dans les poissons sont supérieures aux normes et aux recommandations sanitaires alimentaires de l’Union Européenne.
-						En 2012, les sandres pêchés dans le lac d’Hourtin sont alors interdits à la consommation. En mars 2012 est mise en place une étude complémentaire sur les lacs d’Aquitaine. L’Agence Régionale de la Santé saisit l’ANSES (Agence nationale de sécurité sanitaire de l'alimentation, de l'environnement et du travail) pour cette étude. La fédération de pêche de Gironde et les AAPPMA (les Associations Agréées de Pêche et de Protection du Milieu Aquatique en France) effectuent des campagnes de pêche. Les échantillons de poissons s’étalent sur neuf mois, de juin 2012 à février 2013, et sont contrôlés par les DDTM (Direction Départementale des Territoires et de la Mer) des Landes et de Gironde.
-						A la suite de cette étude, l’ANSES rend un avis en juin 2013 relatif à la contamination par le mercure (Hg) des poissons des lacs médocains (Hourtin-Carcans et Lacanau) et landais (Cazau-Sanguinet, Parentis-Biscarrosse, Aureilhan).
-						En juillet 2013, de nouvelles mesures de gestion sanitaires sont prises. Les sandres du lac de Lacanau sont interdits à la consommation.
-						En 2014, le projet CLAQH est mis en place pour une durée de 4 ans.
+						Le projet prend ses origines au début des années 2010. Tout d’abord, à <B>l’automne 2011</B>, 25 kilos de poissons sont prélevés dans le lac d’Hourtin-Carcans, sous l’impulsion de l’association de Pêche de Hourtin, et confiés au Bureau Veritas pour des analyses. Il s’avère que les quantités de mercure retrouvées dans les poissons sont supérieures aux normes et aux recommandations sanitaires alimentaires de l’Union Européenne. <br/>
+						En <B>2012</B>, les sandres pêchés dans le lac d’Hourtin sont alors interdits à la consommation. En <B>mars 2012</B> est mise en place une étude complémentaire sur les lacs d’Aquitaine. L’Agence Régionale de la Santé saisit l’ANSES (Agence nationale de sécurité sanitaire de l'alimentation, de l'environnement et du travail) pour cette étude. La fédération de pêche de Gironde et les AAPPMA (les Associations Agréées de Pêche et de Protection du Milieu Aquatique en France) effectuent des campagnes de pêche. Les échantillons de poissons s’étalent sur neuf mois, de <B>juin</B> 2012 à février 2013, et sont contrôlés par les DDTM (Direction Départementale des Territoires et de la Mer) des Landes et de Gironde. <br/>
+						A la suite de cette étude, l’ANSES rend un avis en <B>juin 2013</B> relatif à la contamination par le mercure (Hg) des poissons des lacs médocains (Hourtin-Carcans et Lacanau) et landais (Cazau-Sanguinet, Parentis-Biscarrosse, Aureilhan). <br/>
+						En <B>juillet 2013</B>, de nouvelles mesures de gestion sanitaires sont prises. Les sandres du lac de Lacanau sont interdits à la consommation. <br/>
+						En <B>2014</B>, le projet CLAQH est mis en place pour une durée de 4 ans.
 					</p>
 				</div>
 			</div>

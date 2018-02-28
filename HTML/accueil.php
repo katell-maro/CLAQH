2e54a5a42e998b021de43f533bcfe2fd107b6267
Unknown <abouquet001@ensc.fr>
2018-02-28 19:11:49 +0100

    [Accueil] reglage conflit

--- HTML/accueil.php
+++ HTML/accueil.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html>
-<<<<<<< Updated upstream
 <head>
 	<meta charset="UTF-8"/>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -25,30 +24,5 @@
 	<script src="../bootstrap/js/bootstrap.min.js"></script>
 	<script src="lib/jquery/jquery.min.js"></script>
 </body>
-=======
-	<head>
-		<meta charset="UTF-8"/>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title> CLAQH </title>
-		<link rel="icon" href="../IMG/logoTransparentSansTitre.png"  type="image/png">
-		<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
-		<link rel="stylesheet" href="../CSS/style.css">
-		<link rel="stylesheet" href="../CSS/accueil_style.css">
-		<link rel="stylesheet" href="../CSS/nav.css">
-	</head>
-
-
-	<body>
-        <?php include_once 'navigation.html'; ?>
-		<div class="titre">
-			<h1> CLAQH </h1>
-			<p> Contamination polymétallique des Lacs Aquitains et impacts Humains <p>
-			<p> Projet de recherche aquitain<p>
-		</div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <script src="../bootstrap/js/bootstrap.min.js"></script>
-	</body>
->>>>>>> Stashed changes
 
 </html>

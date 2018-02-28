8859636073d27ee6c1b9f2f156db40c13dc07188
Katell <katell.maro@gmail.com>
2018-02-28 18:07:57 +0100

    [Accueil] Centrer le texte

--- HTML/accueil.php
+++ HTML/accueil.php
@@ -1,28 +1,28 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8"/>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title> CLAQH </title>
-		<link rel="icon" href="../IMG/logoTransparentSansTitre.png"  type="image/png">
-		<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
-		<link rel="stylesheet" href="../CSS/style.css">
-		<link rel="stylesheet" href="../CSS/accueil_style.css">
-	</head>
+<head>
+	<meta charset="UTF-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title> CLAQH </title>
+	<link rel="icon" href="../IMG/logoTransparentSansTitre.png"  type="image/png">
+	<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../CSS/style.css">
+	<link rel="stylesheet" href="../CSS/accueil_style.css">
+</head>
 
 
-	<body>
-    <?php include_once 'navigation.html'; ?>
-		<div class="titre">
-			<h1>CLAQH</h1>
-			<p>Contamination polymétallique des Lacs Aquitains et impacts Humains<p>
-			<p>Projet de recherche aquitain<p>
-		</div>
+<body>
+	<?php include_once 'navigation.html'; ?>
+	<div class="titre">
+		<h1>CLAQH</h1>
+		<p>Contamination polymétallique des Lacs Aquitains et impacts Humains</p>
+		<p>Projet de recherche aquitain</p>
+	</div>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="../bootstrap/js/bootstrap.min.js"></script>
-    <script src="lib/jquery/jquery.min.js"></script>
-	</body>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="../bootstrap/js/bootstrap.min.js"></script>
+	<script src="lib/jquery/jquery.min.js"></script>
+</body>
 
 </html>

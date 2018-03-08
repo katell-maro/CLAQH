bb9995c44318a9b2e79fad29b0995430a4ede9d5
Alice <acantagrel@ensc.fr>
2018-03-08 19:24:53 +0100

    [enjeux] correction barre de navigation

--- HTML/enjeux.php
+++ HTML/enjeux.php
@@ -33,10 +33,7 @@
 			</div>
 		</div>
 
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <script src="../bootstrap/js/bootstrap.min.js"></script>
-
-
+		
 		<?php include_once 'footer.php'; ?>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<script src="../bootstrap/js/bootstrap.min.js"></script>

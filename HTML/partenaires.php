70cd1ea60c90291db7eae105951e4e03f01b298b
Alice <acantagrel@ensc.fr>
2018-04-03 16:51:45 +0200

    [partenaire] ajout footer

--- HTML/partenaires.php
+++ HTML/partenaires.php
@@ -30,6 +30,7 @@
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script src="../bootstrap/js/bootstrap.min.js"></script>
+		<?php include_once 'footer.php'; ?>
 
 	</body>
 

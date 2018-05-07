a0d8e87660352246298c4047ecd38c2d815fd690
Katell <katell.maro@gmail.com>
2018-05-07 14:48:09 +0200

    [Définition] Historique + Enjeux + Lacs + Volet 1

--- HTML/enjeux.php
+++ HTML/enjeux.php
@@ -3,6 +3,7 @@
 	<head>
 		<?php include_once 'head.php'; ?> 
 		<title> CLAQH - Enjeux</title>
+		<script src="../js/volet.js"></script>
 	</head>
 
 
@@ -13,26 +14,26 @@
 			<h1> Enjeux </h1>
 
 			<p>
-				Le projet CLAQH vise à étudier et comprendre les raisons de la présence de métaux traces au sein des écosystèmes lacustres du littoral aquitain.
+				Le projet CLAQH vise à étudier et comprendre les raisons de la présence de métaux traces au sein des écosystèmes  <span class="aDefinir" onmouseover="definitionVisible(this);" onmouseleave="definitionCache(this)">lacustres <span class="definition cache">Relatif au lac</span></span> du littoral aquitain.
 			</p>
 
 			<p>
 				Diverses questions sont abordées. Par exemple :  
 				<ul>
 					<li> 
-						Comment se répartit ce mercure au sein de chaque écosystème et pourquoi certaines espèces de poissons et certains lacs semblent plus concernés que d’autres par la présence des métaux traces (volet 1) ?
+						Comment se répartit ce mercure au sein de chaque écosystème et pourquoi certaines espèces de poissons et certains lacs semblent plus concernés que d’autres par la présence des  <span class="aDefinir" onmouseover="definitionVisible(this);" onmouseleave="definitionCache(this)">métaux traces <span class="definition cache"> Ensemble des métaux suivants : l’aluminium, l’arsenic, le cadmium, le chrome, le cuivre, l’étain, le fer, le manganèse, le mercure, le nickel, le plomb, le sélénium et le zinc </span></span> (volet 1) ?
 					</li>
 					<li>
-						A quel moment et comment les métaux traces se transforment-ils pour pénétrer dans les organismes aquatiques (volet 2) ?
+						A quel moment et comment les  <span class="aDefinir" onmouseover="definitionVisible(this);" onmouseleave="definitionCache(this)">métaux traces <span class="definition cache"> Ensemble des métaux suivants : l’aluminium, l’arsenic, le cadmium, le chrome, le cuivre, l’étain, le fer, le manganèse, le mercure, le nickel, le plomb, le sélénium et le zinc </span></span> se transforment-ils pour pénétrer dans les organismes aquatiques (volet 2) ?
 					</li>
 					<li>
 						Quelle est l’origine de la contamination de ces poissons par le mercure (volet 3) ?
 					</li>
 					<li>
-						Quel est le rôle des plantes aquatiques dans la production de nouvelles formes chimiques des métaux traces (volet 4) ?
+						Quel est le rôle des plantes aquatiques dans la production de nouvelles formes chimiques des  <span class="aDefinir" onmouseover="definitionVisible(this);" onmouseleave="definitionCache(this)">métaux traces <span class="definition cache"> Ensemble des métaux suivants : l’aluminium, l’arsenic, le cadmium, le chrome, le cuivre, l’étain, le fer, le manganèse, le mercure, le nickel, le plomb, le sélénium et le zinc </span></span> (volet 4) ?
 					</li>
 					<li>
-						Quel est le rôle des sédiments dans la production de nouvelles formes chimiques et le transfert des métaux traces (volet 5) ?
+						Quel est le rôle des sédiments dans la production de nouvelles formes chimiques et le transfert des  <span class="aDefinir" onmouseover="definitionVisible(this);" onmouseleave="definitionCache(this)">métaux traces <span class="definition cache"> Ensemble des métaux suivants : l’aluminium, l’arsenic, le cadmium, le chrome, le cuivre, l’étain, le fer, le manganèse, le mercure, le nickel, le plomb, le sélénium et le zinc </span></span> (volet 5) ?
 					</li>
 					<li>
 						Quels impacts cette pollution peut avoir sur la population humaine et quelle est la perception des consommateurs (volet 6) ?

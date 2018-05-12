56ea74a067feb9648728b073d02ecc9264763b9f
Katell <katell.maro@gmail.com>
2018-05-12 10:44:06 +0200

    Légers réparages
    Partenaires + définitions + guillemets volet 5

--- HTML/partenaires.php
+++ HTML/partenaires.php
@@ -3,6 +3,7 @@
 	<head>
 		<?php include_once 'head.php'; ?> 
 		<title> CLAQH - Partenaires</title>
+		<script src="../js/volet.js"></script>
 	</head>
 
 
@@ -20,6 +21,8 @@
 				<div class="col-md-9">
 					<p>
 						L’agence de l’eau Adour-Garonne est un établissement public de l’État. Elle a pour missions de lutter contre la pollution et de protéger l’eau et les milieux aquatiques. Il existe en France six agences de l'eau. Dotées de la personnalité civile et de l'autonomie financière, elles sont placées sous double tutelle : celle du ministère de la Transition Ecologique et Solidaire et celle du ministère de l’Économie et des Finances.
+						<br>
+						<a href="http://www.eau-adour-garonne.fr">http://www.eau-adour-garonne.fr</a>
 					</p>
 				</div>
 			</div>
@@ -31,6 +34,8 @@
 				<div class="col-md-9">
 					<p>
 						Irstea est un établissement public à caractère scientifique et technologique (EPST). Réparties en France dans 9 centres, ses activités de recherches et d’expertises sont tournées vers l’action et l’appui aux politiques publiques.
+						<br>
+						<a href="http://www.irstea.fr">http://www.irstea.fr</a>
 					</p>
 				</div>
 			</div>
@@ -41,7 +46,9 @@
 				</div>
 				<div class="col-md-9">
 					<p>
-						L'UMR 5805 « Environnements et Paléoenvironnement Océaniques » (EPOc) a été créée au 1er janvier 1997. Cette UMR a de nombreuses associations avec des laboratoires de l'Université de Bordeaux. 
+						L'<span class="aDefinir" onmouseover="definitionVisible(this);" onmouseleave="definitionCache(this)">UMR<span class="definition cache">Unité Mixte de Recherche</span></span> 5805 « Environnements et Paléoenvironnement Océaniques » (EPOc) a été créée au 1er janvier 1997. Cette UMR a de nombreuses associations avec des laboratoires de l'Université de Bordeaux.
+						<br>
+						<a href="http://www.epoc.u-bordeaux.fr">http://www.epoc.u-bordeaux.fr</a>
 					</p>
 				</div>
 			</div>
@@ -52,7 +59,9 @@
 				</div>
 				<div class="col-md-9">
 					<p>
-						L’ENSC propose des formations scientifiques et managériales solides, ouvertes sur l’international, axées sur la recherche et l’innovation, en interaction étroite avec les entreprises. Précurseur sur les questions d’innovations des technologies numériques et de leurs usages dans les systèmes complexes (santé, énergie, transport, industrie, etc.). L’école a pour ambition de former des ingénieurs aux valeurs éthiques et environnementales affirmées, en plaçant l’homme au centre de l’innovation. 
+						L’ENSC propose des formations scientifiques et managériales solides, ouvertes sur l’international, axées sur la recherche et l’innovation, en interaction étroite avec les entreprises. Précurseur sur les questions d’innovations des technologies numériques et de leurs usages dans les systèmes complexes (santé, énergie, transport, industrie, etc.), l’école a pour ambition de former des ingénieurs aux valeurs éthiques et environnementales affirmées, en plaçant l’homme au centre de l’innovation.
+						<br>
+						<a href="https://ensc.bordeaux-inp.fr">https://ensc.bordeaux-inp.fr</a>
 					</p>
 				</div>
 			</div>
@@ -63,7 +72,9 @@
 				</div>
 				<div class="col-md-9">
 					<p>
-						Dans le champ de l'environnement, le laboratoire d'excellence COTE a pour objectifs d'élaborer des outils permettant de comprendre et prédire l'évolution des écosystèmes et de développer des méthodes de gestion adaptative et de gouvernance pour assurer leur durabilité. 
+						Dans le champ de l'environnement, le laboratoire d'excellence COTE a pour objectifs d'élaborer des outils permettant de comprendre et prédire l'évolution des écosystèmes et de développer des méthodes de gestion adaptative et de gouvernance pour assurer leur durabilité.
+						<br>
+						<a href="https://cote.labex.u-bordeaux.fr">https://cote.labex.u-bordeaux.fr</a>
 					</p>
 				</div>
 			</div>
@@ -75,7 +86,9 @@
 				<div class="col-md-9">
 					<p>
 						 L’université de Bordeaux cultive depuis 575 ans les valeurs universelles qui ont forgé sa réputation : humanisme, exigence, créativité et diversité. L’école des savoirs encyclopédiques a évolué au fil du temps en une université pluridisciplinaire. Elle a su s’adapter à son environnement et se réinventer pour devenir une référence de l’enseignement supérieur et de la recherche.
-					</p>
+						 <br>
+						 <a href="https://www.u-bordeaux.fr">https://www.u-bordeaux.fr</a>
+					</p></a>
 				</div>
 			</div>
 
@@ -86,6 +99,8 @@
 				<div class="col-md-9">
 					<p>
 						 L’université de Pau et des pays de l’Adour (UPPA) est un établissement supérieur français multi-sites fondé en 1972 et basé dans le bassin de l’Adour ; principalement à Pau mais aussi à Bayonne et Anglet dans les Pyrénées-Atlantiques, à Tarbes dans les Hautes-Pyrénées et à Mont-de-Marsan dans les Landes. Elle dépend de l'académie de Bordeaux.
+						 <br>
+						 <a href="https://www.univ-pau.fr">https://www.univ-pau.fr</a>
 					</p>
 				</div>
 			</div>

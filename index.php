f0f35f02f6df243bc8141d20a879f9e0bdd8cd25
Katell <katell.maro@gmail.com>
2018-05-28 21:31:46 +0200

    [Organisation] Css navbar

--- index.php
+++ index.php
@@ -23,7 +23,6 @@
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="LIB/Bootstrap/js/bootstrap.min.js"></script>
-	<!--<script src="lib/jquery/jquery.min.js"></script>-->
 </body>
 
 </html>
